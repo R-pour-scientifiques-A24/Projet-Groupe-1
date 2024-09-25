d23a8f8f2c8fd0cbe092907248ceb88e4071c02b
Anne-Marie Groulx <AMGRO10@ulaval.ca>
2024-09-25 15:10:41 -0400

    Nettoyage données 1

---
+++ extra/nettoyagedonnées.R
@@ -0,0 +1,13 @@
+
+volcan1<-merge(eruptions,events, by="eruption_number")
+View(volcan1)
+volcan<-merge(volcan1,volcano, by.x="volcano_number.x", by.y="volcano_number")
+View(volcan)
+
+sum(duplicated(volcan$event_number)) #C'est bon! On a une ligne par no. d'événement
+
+volcan<-volcan[, -c(8,9,12,13,16,17,18,21,23,24,25,31,32,37:40,42:45)]
+View(volcan)
+
+#volcan<-volcan[, c(8,9,12,13,16,17,18,21,23,24,25,31,32,37:40,42:45)]
+#On peut vérifier qu'on a retiré les bonnes colonnes!
