67d1ec78770a66e6d51d0fa31d90df3b6b55358e
Eraud16 <117849420+Eraud16@users.noreply.github.com>
2024-09-26 09:14:31 -0400

    Renommer les variables mal nommées

--- extra/nettoyagedonnées.R
+++ extra/nettoyagedonnées.R
@@ -1,4 +1,4 @@
-
+ 
 volcan1<-merge(eruptions,events, by="eruption_number")
 View(volcan1)
 volcan<-merge(volcan1,volcano, by.x="volcano_number.x", by.y="volcano_number")
@@ -11,3 +11,8 @@ View(volcan)
 
 #volcan<-volcan[, c(8,9,12,13,16,17,18,21,23,24,25,31,32,37:40,42:45)]
 #On peut vérifier qu'on a retiré les bonnes colonnes!
+
+colnames(volcan)[1] <- "volcano_number"
+colnames(volcan)[3] <- "volcano_name"
+colnames(volcan)[10] <- "latitude"
+colnames(volcan)[11] <- "longitude"
