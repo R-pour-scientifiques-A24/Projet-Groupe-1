5e1d9cc26f8899b6962042371fec9ce94599ac80
Allisson Picard <ALPIC88@ulaval.ca>
2024-11-20 16:06:45 -0500

    fonction catego

--- package/fct_catego.R
+++ package/fct_catego.R
@@ -1,10 +1,16 @@
-library(readr)
 
 fct_catego <- function(x){
-  if (!(is.factor(x) & is.vector(x))){
+  if (!(is.factor(x))){
     stop(" L'argument fourni n'est pas de type Facteur.")
   } else {
-    
+    liste_statcateg<-list(nombre_observations=list(length(x), sum(is.na(x))), 
+                    frequences=data.frame(Modalite=levels(x), Frequence=table(x), Proportion=prop.table(table(x))),
+                    mode=which.max(table(x)), observations=x)
+    class(liste_statcateg) <- "statcateg"
+    return(liste_statcateg)
   }
 }
+
+categ<-fct_catego(volcan$eruption_category)
+str(categ)
 
