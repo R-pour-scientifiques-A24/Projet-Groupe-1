ae3495f3828a8dbd13abcaaf29e3a705a7b35e17
Allisson Picard <ALPIC88@ulaval.ca>
2024-11-20 14:49:02 -0500

    fonction numerique

--- package/fonction_numerique.R
+++ package/fonction_numerique.R
@@ -3,7 +3,11 @@ fct_numerique <- function(x){
   if (!(is.numeric(x) & is.vector(x))){
     stop("L'argument fourni n'est pas de type Numerique.")
   } else {
-    
+     liste_sd <- list(nrow(x), length(is.na(x)), quantile(x, probs = c(0, 0.1, 0.25, 0.5, 0.75, 0.9, 1) , na.rm=TRUE),
+                       mean(x, na.rm=TRUE), sd(x, na.rm=TRUE), return(x))
+     class(liste_sd) <- "sd"
+     return(liste_sd)
   }
 }
 
+fct_numerique(volcan$start_year)
