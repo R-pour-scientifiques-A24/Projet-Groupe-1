c396f4bf715c7a44c4071fdb9f0317dae342375a
Anne-Marie Groulx <AMGRO10@ulaval.ca>
2024-11-20 16:36:37 -0500

    statcateg freq

--- package/fct_catego.R
+++ package/fct_catego.R
@@ -1,10 +1,15 @@
-
 fct_catego <- function(x){
   if (!(is.factor(x))){
     stop(" L'argument fourni n'est pas de type Facteur.")
   } else {
+    
+    freq<-as.matrix(rbind(names(table(volcan$eruption_category)), table(volcan$eruption_category), prop.table(table(volcan$eruption_category))))
+    freq<-as.data.frame(t(freq))
+    colnames(freq)<-c("Modalités","Fréquences", "Proportion")
+    rownames(freq) <- NULL
+    
     liste_statcateg<-list(nombre_observations=list(length(x), sum(is.na(x))), 
-                    frequences=data.frame(Modalite=levels(x), Frequence=table(x), Proportion=prop.table(table(x))),
+                    frequences=freq,
                     mode=which.max(table(x)), observations=x)
     class(liste_statcateg) <- "statcateg"
     return(liste_statcateg)
@@ -15,7 +20,7 @@ categ<-fct_catego(volcan$eruption_category)
 str(categ)
 
 
-# x : Un objet de type statnum 
+# x : Un objet de type statcateg
 print.statcateg<-function(x){
   if (class(x)!="statcateg"){
     stop("L'argument fourni n'est pas de classe statcateg.")
@@ -26,3 +31,17 @@ print.statcateg<-function(x){
   }
 }
 print(categ)
+
+# x : Un objet de type statcateg
+summary.statcateg<-function(x){
+  if (class(x)!="statcateg"){
+    stop("L'argument fourni n'est pas de classe statcateg.")
+  }else{
+    cat("Variable catégoriquee\n\nNombre d'observations:\n")
+    cat("Total:", as.character(x[[1]][1]), "\nManquantes(NA):", as.character(x[[1]][2]))
+    cat("\n\nFréquences:")
+    as.data.frame(x[[2]])
+  }
+}
+summary(categ)
+
