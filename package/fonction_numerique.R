03d256d5427101b7e1d42119428781df353a6df6
Allisson Picard <ALPIC88@ulaval.ca>
2024-11-20 14:02:59 -0500

    début fonction A

--- package/fonction_numerique.R
+++ package/fonction_numerique.R
@@ -1,7 +1,9 @@
+
 fct_numerique <- function(x){
-  if (!is.numeric(x)){
+  if (!(is.numeric(x) & is.vector(x))){
     stop("L'argument fourni n'est pas de type Numerique.")
   } else {
     
   }
 }
+
