dcc1a23bfebe585fed7fc490518a00237670f068
Anne-Marie Groulx <AMGRO10@ulaval.ca>
2024-11-20 14:59:53 -0500

    testok

--- package/fonction_numerique.R
+++ package/fonction_numerique.R
@@ -1,13 +1,13 @@
 
 fct_numerique <- function(x){
   if (!(is.numeric(x) & is.vector(x))){
-    stop("L'argument fourni n'est pas de type Numerique.")
+    stop("L'argument fourni n'est pas de type numerique.")
   } else {
-     liste_sd <- list(nrow(x), length(is.na(x)), quantile(x, probs = c(0, 0.1, 0.25, 0.5, 0.75, 0.9, 1) , na.rm=TRUE),
-                       mean(x, na.rm=TRUE), sd(x, na.rm=TRUE), return(x))
+    liste_sd<-list(list(length(x), sum(is.na(x))), quantile(x, probs = c(0, 0.1, 0.25, 0.5, 0.75, 0.9, 1) , na.rm=TRUE),
+         mean(x, na.rm=TRUE), sd(x, na.rm=TRUE), x)
      class(liste_sd) <- "sd"
-     return(liste_sd)
   }
 }
 
-fct_numerique(volcan$start_year)
+year<-fct_numerique(volcan$start_year)
+str(year)
