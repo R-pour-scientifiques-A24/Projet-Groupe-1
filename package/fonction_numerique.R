16699511f8567417ffeb72ab7e158bb2b0dd6c0c
Anne-Marie Groulx <AMGRO10@ulaval.ca>
2024-11-20 15:26:10 -0500

    return num

--- package/fonction_numerique.R
+++ package/fonction_numerique.R
@@ -3,12 +3,19 @@ fct_numerique <- function(x){
     stop("L'argument fourni n'est pas de type numerique.")
   } else {
     liste_sd<-list(list(length(x), sum(is.na(x))), quantile(x, probs = c(0, 0.1, 0.25, 0.5, 0.75, 0.9, 1) , na.rm=TRUE),
-         mean(x, na.rm=TRUE), sd(x, na.rm=TRUE), x)
-     class(liste_sd) <- "sd"
+              mean(x, na.rm=TRUE), sd(x, na.rm=TRUE), x)
+     
+    class(liste_sd) <- "sd"
+    
+    return(liste_sd)
   }
 }
 
-fct_numerique(volcan$start_year)
 year<-fct_numerique(volcan$start_year)
 str(year)
+print(year)
 
+
+print.sd<-function(x){
+  
+}
