005425557b28660648b6c40e95104eb7eae2d4bf
Allisson Picard <ALPIC88@ulaval.ca>
2024-11-27 16:21:30 -0500

    fonction principale

--- package/fonction_principale.R
+++ package/fonction_principale.R
@@ -22,24 +22,25 @@ fct_principale <- function(x){
     stop("L'argument fourni n'est pas un data frame.")
   } else{
     liste <- list()
+    matrice_nb_obs <- matrix(NA, nrow=2, ncol=ncol(x))
+    rownames(matrice_nb_obs) <- c( "Total :", "Manquantes (NA) :")
+    matrice_type_variable <- matrix(NA,nrow=1,ncol=col(x))
+    matrice_stat_num <- NULL
     for (colonne in 1:ncol(x)){
-      if (is.numeric(colonne) & is.vector(colonne)){
-        num <- fct_numerique(colonne)
-        nombre_observations <- fct_numerique(colonne)[1]
-        type_variable <- "Numerique"
-        stat_num <- num
+      if (is.numeric(x[,colonne]) & is.vector(x[,colonne])){
+        matrice_nb_obs[,colonne] <- as.matrix(fct_numerique(x[,colonne])[[1]])
+        matrice_type_variable[,colonne] <- "Numerique"
+        matrice_stat_num <- cbind(matrice_stat_num, fct_numerique(x[,colonne])[c(2,3,4)])
       }
-      if (is.factor(colonne)){
+      if (is.factor(x[,colonne])){
         catego <- fct_catego(colonne)
-        nombre_observations <- fct_catego(colonne)[1]
-        type_variable <- "Facteur"
+        matrice_nb_obs[,colonne] <- as.matrix(fct_catego(x[,colonne])[[1]])
+        matrice_type_variable[,colonne] <- "Facteur"
         stat_fact <- list(catego[2], catego[3])
       } else {
         class(colonne) <- "autre"
-        nombre_observations <- list(nombre_observations=list(length(colonne), 
-                                                             sum(is.na(colonne))))
-        type_variable <- "Autre"
-        var_autre <- names(colonne)
+        matrice_nb_obs[,colonne] <- c("Total"=length(x[,colonne]), "Manquantes (NA)"=sum(is.na(x[,colonne])))
+        matrice_type_variable[,colonne] <- "Autre"
       }
     }
   }
