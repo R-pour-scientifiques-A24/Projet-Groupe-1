2edda910e7984151a8e2871f6fcb40b4aed79197
Allisson Picard <ALPIC88@ulaval.ca>
2024-11-27 15:15:41 -0500

    fct catego corrigée

--- package/fonction_principale.R
+++ package/fonction_principale.R
@@ -1,8 +1,8 @@
-fonction_principale <- function(x){
+fct_principale <- function(x){
   if (!is.data.frame(x)){
     stop("L'argument fourni n'est pas un data frame.")
   } else{
-    for (colonne in 1:length(x)){
+    for (colonne in 1:ncol(x)){
       if (is.numeric(colonne) & is.vector(colonne)){
         num <- lapply(x, FUN=fct_numerique(colonne))
       }
@@ -15,7 +15,35 @@ fonction_principale <- function(x){
   }
 }
 
-test <- fonction_principale(volcan)
+test <- fct_principale(volcan)
 str(test)
 
+
+fct_principale <- function(x){
+  if (!is.data.frame(x)){
+    stop("L'argument fourni n'est pas un data frame.")
+  } else{
+    liste <- list()
+    for (colonne in 1:ncol(x)){
+      if (is.numeric(colonne) & is.vector(colonne)){
+        num <- fct_numerique(colonne)
+        nombre_observations <- fct_numerique(colonne)[1]
+        type_variable <- "Numerique"
+        stat_num <- num
+      }
+      if (is.factor(colonne)){
+        catego <- fct_catego(colonne)
+        nombre_observations <- fct_catego(colonne)[1]
+        type_variable <- "Facteur"
+        stat_fact <- list(catego[2], catego[3])
+      } else {
+        class(colonne) <- "autre"
+        nombre_observations <- list(nombre_observations=list(length(colonne), 
+                                                             sum(is.na(colonne))))
+        type_variable <- "Autre"
+        var_autre <- names(colonne)
+      }
+    }
+  }
+}
 
