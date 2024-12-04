ebc7322d3b635ddae9cd7dd5e8f23ed5f7dd0a89
Erika Audet <ERAUD16@ulaval.ca>
2024-12-04 16:23:40 -0500

    rester a faire chose

--- package/fonctions_brouillon.R
+++ package/fonctions_brouillon.R
@@ -1,37 +1,45 @@
 #######################################################################
-fct_principale <- function(x){
+fct_principale<- function(x){
   if (!is.data.frame(x)){
     stop("L'argument fourni n'est pas un data frame.")
   } else{
     liste <- list()
-    matrice_nbobs<- matrix(NA, nrow=2, ncol=ncol(x))
-    rownames(matrice_nbobs)<- c("Total", "Manquantes (NA)")
-    matrice_type <- matrix(NA, nrow=1, ncol=ncol(x))
-    matrice_statnum <- NULL
-    liste_statfreq <- list()
-    
-    
+    matrice_nb_obs <- as.data.frame(matrix(NA, nrow=2, ncol=ncol(x)))
+    rownames(matrice_nb_obs) <- c( "Total :", "Manquantes (NA) :")
+    matrice_type_variable <- as.data.frame(matrix(NA,nrow=1,ncol=ncol(x)))
+    nb_num <- sum(sapply(x, is.numeric))
+    matrice_stat_num <- as.data.frame(matrix(NA,nrow=9,ncol=nb_num))
+    nom_col <- c()
+    liste_stat_fact <- list()
+    vecteur_var_autre <- c()
     for (colonne in 1:ncol(x)){
       if (is.numeric(x[,colonne]) & is.vector(x[,colonne])){
-        matrice_nbobs[,colonne]<- fct_numerique(x[,colonne])[[1]]
-        matrice_type[,colonne]<- "Numérique"
-        statnum <- c(fct_numerique(x[,colonne])[[2]],fct_numerique(x[,colonne])[[3]],fct_numerique(x[,colonne])[[4]])
-        statnum <- t(statnum)
-        matrice_statnum <- cbind(matrice_statnum,statnum)
-        
+        matrice_nb_obs[,colonne] <- as.matrix(fct_numerique(x[,colonne])[[1]])
+        colnames(matrice_nb_obs)[colonne] <- colnames(x)[colonne]
+        matrice_type_variable[,colonne] <- "Numerique"
+        colnames(matrice_type_variable)[colonne] <- colnames(x)[colonne]
+        stat_num <- c(fct_numerique(x[,colonne])[[2]],fct_numerique(x[,colonne])[[3]],fct_numerique(x[,colonne])[[4]])
+        matrice_stat_num <- cbind(matrice_stat_num, stat_num)
       }
       if (is.factor(x[,colonne])){
-        matrice_nbobs[,colonne]<- fct_catego(x[,colonne])[[1]]
-        matrice_type[,colonne]<- "Catégorique"
-        liste_stat_fact <- c(liste_stat_fact, fct_catego(x[,colonne])[2], fct_catego(x[,colonne])[3])
-        
+        catego <- fct_catego(x[,colonne])
+        matrice_nb_obs[,colonne] <- as.matrix(fct_catego(x[,colonne])[[1]])
+        colnames(matrice_nb_obs)[colonne] <- colnames(x)[colonne]
+        matrice_type_variable[,colonne] <- "Facteur"
+        colnames(matrice_type_variable)[colonne] <- colnames(x)[colonne]
+        liste_stat_fact <- c(liste_stat_fact, catego[[2]], catego[[3]])
       } else {
-        class(colonne) <- "Autre"
-        matrice_nbobs[,colonne]<-c("Total"=length(x[,colonne]), "Manquantes (NA)"=sum(is.na(x[,colonne])))
-        matrice_type[,colonne]<- "Autre"
-        
+        class(x[,colonne]) <- "autre"
+        matrice_nb_obs[,colonne] <- c("Total"=length(x[,colonne]), "Manquantes (NA)"=sum(is.na(x[,colonne])))
+        colnames(matrice_nb_obs)[colonne] <- colnames(x)[colonne]
+        matrice_type_variable[,colonne] <- "Autre"
+        colnames(matrice_type_variable)[colonne] <- colnames(x)[colonne]
+        vecteur_var_autre <- c(vecteur_var_autre, names(x)[colonne])
       }
     }
+    liste_finale <- list(nombre_observations=as.matrix(matrice_nb_obs), type_variable=as.matrix(matrice_type_variable), stat_num=matrice_stat_num, stat_fact=liste_stat_fact, var_autre=vecteur_var_autre)
+    return(liste_finale)
+    class(liste_finale) <- "principale"
   }
 }
 
@@ -65,42 +73,60 @@ fct_principale<- function(x){
     liste <- list()
     matrice_nb_obs <- as.data.frame(matrix(NA, nrow=2, ncol=ncol(x)))
     rownames(matrice_nb_obs) <- c( "Total :", "Manquantes (NA) :")
-    matrice_type_variable <- as.data.frame(matrix(NA,nrow=1,ncol=ncol(x)))
+    matrice_type_variable <- matrix(NA,nrow=1,ncol=ncol(x))
     matrice_stat_num <- NULL
     liste_stat_fact <- list()
+    freq <- list()
+    mode <- list()
     vecteur_var_autre <- c()
     for (colonne in 1:ncol(x)){
       if (is.numeric(x[,colonne]) & is.vector(x[,colonne])){
         matrice_nb_obs[,colonne] <- as.matrix(fct_numerique(x[,colonne])[[1]])
         colnames(matrice_nb_obs)[colonne] <- colnames(x)[colonne]
         matrice_type_variable[,colonne] <- "Numerique"
-        colnames(matrice_type_variable)[colonne] <- colnames(x)[colonne]
-        stat_num <- c(fct_numerique(x[,colonne])[[2]],fct_numerique(x[,colonne])[[3]],fct_numerique(x[,colonne])[[4]])
-        stat_num <- t(stat_num)
-        matrice_stat_num <- cbind(matrice_stat_num,stat_num)
+        stat_num <- matrix(c(fct_numerique(x[,colonne])[[2]],fct_numerique(x[,colonne])[[3]],fct_numerique(x[,colonne])[[4]]),9,1)
+        colnames(stat_num) = colnames(x)[colonne]
+        matrice_stat_num <- (cbind(matrice_stat_num,stat_num))
+        #colnames(matrice_stat_num)[colonne] <- colnames(x)[colonne]
+        #  print(matrice_stat_num)
       }
       if (is.factor(x[,colonne])){
         catego <- fct_catego(x[,colonne])
         matrice_nb_obs[,colonne] <- as.matrix(fct_catego(x[,colonne])[[1]])
-        colnames(matrice_nb_obs)[colonne] <- colnames(x)[colonne]
         matrice_type_variable[,colonne] <- "Facteur"
-        colnames(matrice_type_variable)[colonne] <- colnames(x)[colonne]
-        liste_stat_fact <- c(liste_stat_fact, catego[[2]], catego[[3]])
+        tab_freq <- data.frame(catego[[2]])
+        freq <- c(freq,list(tab_freq))
+        tab_mode <- data.frame(catego[[3]])
+        mode <- c(mode,list(tab_mode))
+        colnames(matrice_nb_obs)[colonne] <- colnames(x)[colonne]
       } else {
         class(x[,colonne]) <- "autre"
         matrice_nb_obs[,colonne] <- c("Total"=length(x[,colonne]), "Manquantes (NA)"=sum(is.na(x[,colonne])))
         colnames(matrice_nb_obs)[colonne] <- colnames(x)[colonne]
         matrice_type_variable[,colonne] <- "Autre"
-        colnames(matrice_type_variable)[colonne] <- colnames(x)[colonne]
         vecteur_var_autre <- c(vecteur_var_autre, names(x)[colonne])
       }
     }
-    liste_finale <- list(nombre_observations=as.matrix(matrice_nb_obs), type_variable=as.matrix(matrice_type_variable), stat_num=matrice_stat_num, stat_fact=liste_stat_fact, var_autre=vecteur_var_autre)
-    return(liste_finale)
+    row.names(matrice_stat_num) = c("0%", "10%", "25%", "50%", "75%", "90%", "100%", "Moyenne", "E.T.")
+    liste_stat_fact <- c(liste_stat_fact, "Fréquences"=list(freq), "Mode"=list(mode))
+    liste_finale <- list(nombre_observations=as.matrix(matrice_nb_obs), type_variable=matrice_type_variable, stat_num=as.matrix(matrice_stat_num), stat_fact=liste_stat_fact, var_autre=vecteur_var_autre)
     class(liste_finale) <- "principale"
+    return(liste_finale)
   }
 }
 
 result <- fct_principale(volcan)
-
 str(result)
+x= volcan$eruption_category
+liste_stat_fact <- list()
+freq <- list()
+mode <- list()
+catego <- fct_catego(x)
+tab_freq <- data.frame(catego[2])
+freq <- c(freq,tab_freq)
+tab_mode <- data.frame(catego[[3]])
+mode <- c(mode,list(tab_mode))
+liste_stat_fact <- c(liste_stat_fact, freq, mode)
+str(liste_stat_fact)
+
+# trouver comment changer le nom data frame et summary
